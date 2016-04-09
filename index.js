c0e216cf0d36b5c96807a9ab5a2b887b56fc4c2f
whricom <whricom@yahoo.com>
2016-04-09 01:28:33 -0400

    added extended true for the urlencoded body parser

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({ secret: "library" }));
 require("./src/config/passport")(app);
